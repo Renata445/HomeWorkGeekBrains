ee8672c01af09790a7c17995e03e315feff8959c
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-11-28 22:16:28 +0300

    Exercise 1

---
+++ Задание_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите 2 числа: ");
+int number_1 = Convert.ToInt32( Console.ReadLine());
+int number_2 = Convert.ToInt32( Console.ReadLine());
+int max = 0;
+if(number_1 > number_2)
+{
+max = number_1;
+}
+else
+{
+max = number_2;
+}
+Console.Write("max = ");
+Console.WriteLine(max);
