64027b5719b780a6fac3cefc934ed897192b484c
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-11-29 20:08:02 +0300

    Exercise_4

---
+++ Exercise_4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for(int i = 1; i <= N; i++)
+{
+    if(i % 2 == 0)
+    {
+        Console.Write($"{i} ");
+    }
+}
