a851929a2738b3476a8b96e60f6ff6215745449f
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-11-29 18:34:50 +0300

    Exercise_2

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Введите 3 числа: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+int number_2 = Convert.ToInt32(Console.ReadLine());
+int number_3 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (number_1 > max)
+{
+    max = number_1;
+}
+if (number_2 > max)
+{
+    max = number_2;
+}
+if (number_3 > max)
+{
+    max = number_3;
+}
+Console.Write("max = ");
+Console.WriteLine(max);
